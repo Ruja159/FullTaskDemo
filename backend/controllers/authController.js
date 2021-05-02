fe52f7aa100c23f694cdfbdf6647d999597c1ed5
Aleksandar Rujevic <ruja_prijedor@hotmail.com>
2021-05-02 16:20:34 +0200

    react init

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,15 +4,18 @@ const jwt = require('jsonwebtoken')
 const accessTokenSecret = require('../config')
 
 const auth = (req, res) => {
+
+
+    const hashedPassword = bcrypt.hashSync(req.body.password, 8)
     const user = data.user.find(u => u.email === req.body.email);
-    if (user) {
+    if (user.password == hashedPassword) {
         const accessToken = jwt.sign({ email: user.email }, accessTokenSecret, { expiresIn: '20m' });
         res.json({
             accessToken
         });
     } else {
-        res.json({msg: "Invalid email or password"});
+        res.json({ msg: "Invalid email or password" });
     }
 }
 
-module.exports = {auth}
+module.exports = { auth }
